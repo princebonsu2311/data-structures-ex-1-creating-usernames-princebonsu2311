a8d73f8a559660c6869ee23748a7a280b036f649
Prince.Bonsu <student.p.bonsu@horizoncolumnus.org>
2019-10-23 08:46:51 -0400

    update

--- username_generator.py
+++ username_generator.py
@@ -1,1 +1,4 @@
 student_names = ["Tyler Fowsantia", "Nana Yeboah", "Hasna Abdullahi", "Tarrison Williams"]
+
+for n in student_names:
+    print("student."+n[0]+"."+n.split(" ")[1]+ "@horizoncolumbus.org")
